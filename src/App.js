8338abd35122ca3ecfdab301907aeffddc72932f
Catherine Gloria <catherinegloria@gmail.com>
2021-04-12 19:56:07 -0700

    add packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from "react";
+import MainPage from "../pages/index.jsx"
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends Component () {
+
+  render() {
+    return (
+      <Router>
+        <Route exact path='/' component={MainPage} />
+      </Router>
+    );
+  }
 }
 
 export default App;
