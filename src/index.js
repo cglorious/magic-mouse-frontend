075b8f10a4f4aec923685661b96a9bec91b05976
Catherine Gloria <catherinegloria@gmail.com>
2021-04-13 16:25:24 -0700

    add folders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import {
   BrowserRouter as Router,
   Route,
@@ -7,12 +9,18 @@ import {
   Link,
   Redirect
 } from "react-router-dom";
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/rootReducer';
 
 import App from './App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
