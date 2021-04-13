a14153e019b13f99231b08804ee14366bde48c4c
Catherine Gloria <catherinegloria@gmail.com>
2021-04-13 16:32:31 -0700

    fetch resorts action

---
+++ src/actions/fetchResorts.js
@@ -0,0 +1,11 @@
+export function fetchResorts () {
+  return(dispatch) => {
+    fetch('http://localhost:3000/api/v1/resorts')
+    .then(resp => resp.json())
+    .then(resorts => {
+      dispatch({
+        type: 'FETCH_RESORTS',
+        payload: resorts.data})
+    })
+  }
+}
