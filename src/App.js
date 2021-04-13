36b8ac1f9f9082f1f36c519c34e3d6f4a340d7aa
Catherine Gloria <catherinegloria@gmail.com>
2021-04-12 20:27:22 -0700

    stub pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import MainPage from "./pages/index"
-import { Route } from "react-router-dom";
+import UsersPage from "./pages/users";
+import NotFoundPage from "./pages/404";
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <Route path='/' render={() => <MainPage />} />
+      <Router>
+      <Switch>
+        <Route exact path='/' render={() => <MainPage />} />
+        <Route exact path='/users' render={() => <UsersPage />} />
+        <Route exact path='/404' render={() => <NotFoundPage />} />
+        <Redirect to='/404' />
+      </Switch>
+      </Router>
       </div>
     );
   }
