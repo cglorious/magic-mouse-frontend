0abbe84d192098e18701e8e572ce7cd5cb8bc7ca
Catherine Gloria <catherinegloria@gmail.com>
2021-04-13 17:20:29 -0700

    resort component

--- src/components/Resort.js
+++ src/components/Resort.js
@@ -6,6 +6,9 @@ const Resort = props => {
     return(
       <div>
         <p><Link to={props.slug}>{props.name}</Link></p>
+        <img src={props.image} alt={props.name}/>
+        <p>{props.location}</p>
+        <hr/>
       </div>
 
     );
