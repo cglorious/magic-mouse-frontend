e84f55f444997763bf87620acd73c64da93bc8a7
Catherine Gloria <catherinegloria@gmail.com>
2021-04-13 17:11:35 -0700

    hard code resort components in resort

--- src/components/Resorts.js
+++ src/components/Resorts.js
@@ -3,9 +3,11 @@ import Resort from './Resort';
 
 const Resorts = ({resorts}) => {
 
+//function - iterate through data and create Resort components
+
   return (
     <div className="App">
-      <div className="resorts-list">
+      <h3>Where would you like to travel?</h3>
       <Resort
         slug="/disneyland-resort"
         key="1"
@@ -13,9 +15,20 @@ const Resorts = ({resorts}) => {
         location="Anaheim, CA USA"
         image="https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/2/1280/720/75/dam/disneyland/home/destination/sleeping-beauty-castle-tint-16x9.jpg"
         />
-        <p>Disneyworld Resort</p>
-        <p>Toyko Disney Resort</p>
-      </div>
+      <Resort
+        slug="/disneyworld-resort"
+        key="2"
+        name="Walt Disney Resort"
+        location="Orlando, FL, USA"
+        image="https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2020/02/bcc92834982402984029834.jpg"
+          />
+      <Resort
+        slug="/tokyo-disney-resort"
+        key="3"
+        name="Tokyo Disney Resort"
+        location="Tokyo, Japan"
+        image="https://www.slashfilm.com/wp/wp-content/images/tokyo-disneyland-e1592926525256.jpeg"
+        />
     </div>
   );
 };
