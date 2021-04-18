a2a00dbdb3a45db31b58e5a4b7226bd8deed2633
Catherine Gloria <catherinegloria@gmail.com>
2021-04-17 19:35:38 -0700

    update root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import resortsReducer from './resortsReducer';
+import parksReducer from './parksReducer';
 
 const rootReducer = combineReducers({
-  resorts: resortsReducer
+  resorts: resortsReducer,
+  parks: parksReducer
 });
 
 export default rootReducer;
