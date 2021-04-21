19f54993886b546140af37a367d2615e4993b6aa
Catherine Gloria <catherinegloria@gmail.com>
2021-04-20 20:40:38 -0700

    actions for load attractions for land

--- src/actions/loadAttractionsForLand.js
+++ src/actions/loadAttractionsForLand.js
@@ -21,3 +21,87 @@ export const fetchFantasyland = () => {
     })
   }
 }
+
+export const fetchTomorrowland = () => {
+  return(dispatch) => {
+    fetch(`http://localhost:3000/api/v1/lands/3`)
+    .then(resp => resp.json())
+    .then(land => {
+      dispatch({
+        type: 'LOAD_ATTRACTIONS_FOR_TOMORROWLAND',
+        payload: land.data.attributes.attractions})
+    })
+  }
+}
+
+export const fetchAdventureland = () => {
+  return(dispatch) => {
+    fetch(`http://localhost:3000/api/v1/lands/4`)
+    .then(resp => resp.json())
+    .then(land => {
+      dispatch({
+        type: 'LOAD_ATTRACTIONS_FOR_ADVENTURELAND',
+        payload: land.data.attributes.attractions})
+    })
+  }
+}
+
+export const fetchCritterCountry = () => {
+  return(dispatch) => {
+    fetch(`http://localhost:3000/api/v1/lands/5`)
+    .then(resp => resp.json())
+    .then(land => {
+      dispatch({
+        type: 'LOAD_ATTRACTIONS_FOR_CRITTERCOUNTRY',
+        payload: land.data.attributes.attractions})
+    })
+  }
+}
+
+export const fetchFrontierland = () => {
+  return(dispatch) => {
+    fetch(`http://localhost:3000/api/v1/lands/6`)
+    .then(resp => resp.json())
+    .then(land => {
+      dispatch({
+        type: 'LOAD_ATTRACTIONS_FOR_FRONTIERLAND',
+        payload: land.data.attributes.attractions})
+    })
+  }
+}
+
+export const fetchMainstreetUSA= () => {
+  return(dispatch) => {
+    fetch(`http://localhost:3000/api/v1/lands/7`)
+    .then(resp => resp.json())
+    .then(land => {
+      dispatch({
+        type: 'LOAD_ATTRACTIONS_FOR_MAINSTREETUSA',
+        payload: land.data.attributes.attractions})
+    })
+  }
+}
+
+export const fetchMickeysToonTown= () => {
+  return(dispatch) => {
+    fetch(`http://localhost:3000/api/v1/lands/8`)
+    .then(resp => resp.json())
+    .then(land => {
+      dispatch({
+        type: 'LOAD_ATTRACTIONS_FOR_MICKEYSTOONTOWN',
+        payload: land.data.attributes.attractions})
+    })
+  }
+}
+
+export const fetchNOLA= () => {
+  return(dispatch) => {
+    fetch(`http://localhost:3000/api/v1/lands/9`)
+    .then(resp => resp.json())
+    .then(land => {
+      dispatch({
+        type: 'LOAD_ATTRACTIONS_FOR_NOLA',
+        payload: land.data.attributes.attractions})
+    })
+  }
+}
