ed094d246acf2031bbafbcea4a3aa03a92419d73
Catherine Gloria <catherinegloria@gmail.com>
2021-04-20 20:35:01 -0700

    update 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "../styles/style.css";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h2>404 Not Found</h2>
+    <div className="page-container">
+      <img className= "center-pixie" src="https://i.pinimg.com/originals/fd/f7/14/fdf714eb409e093eae14bde94e9f0648.jpg" alt="Pardon our Pixie Dust" />
+      <br/ >
+      <div className="header">
+        <h1>SORRY, we couldn't find that page.</h1>
+        <h5> Try going back to <a href='/'>Magic Mouse's home page.</a></h5>
+      </div>
     </div>
   );
 };
