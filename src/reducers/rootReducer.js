609abae3af449a59cc52440ab281c4c0ca146157
Catherine Gloria <catherinegloria@gmail.com>
2021-04-20 20:51:39 -0700

    add reducer and container files

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,6 +3,13 @@ import landsReducer from './landsReducer';
 import attractionsReducer from './attractionsReducer';
 import starWarsReducer from './starWarsReducer';
 import fantasylandReducer from './fantasylandReducer';
+// import tomorrowlandReducer from './tomorrowlandReducer';
+// import adventurelandReducer from './adventurelandReducer';
+// import critterCountryReducer from './critterCountryReducer';
+// import frontierlandReducer from './frontierlandReducer';
+// import mainstreetUSAReducer from './mainstreetUSAReducer';
+// import mickeysToontownReducer from './mickeysToontownReducer';
+// import nolaReducer from './nolaReducer';
 
 const rootReducer = combineReducers({
   lands: landsReducer,
