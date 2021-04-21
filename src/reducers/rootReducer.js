4118e6bfb95a2972652a70d060b60fe7041d235e
Catherine Gloria <catherinegloria@gmail.com>
2021-04-20 20:42:00 -0700

    organize reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-import landsReducer from './combine/landsReducer';
-import attractionsReducer from './combine/attractionsReducer';
-import starWarsReducer from './combine/starWarsReducer';
-import fantasylandReducer from './combine/fantasylandReducer';
+import landsReducer from './landsReducer';
+import attractionsReducer from './attractionsReducer';
+import starWarsReducer from './starWarsReducer';
+import fantasylandReducer from './fantasylandReducer';
 
 const rootReducer = combineReducers({
   lands: landsReducer,
