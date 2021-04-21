c1adf2dea62606231f4c48699140efcf834d81cb
Catherine Gloria <catherinegloria@gmail.com>
2021-04-20 21:14:34 -0700

    stub adventureland container

---
+++ src/containers/AdventurelandContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchAdventureland } from '../actions/loadAttractionsForLand'
+// import Fantasyland from '../components/Fantasyland';
+
+class AdventurelandContainer extends Component {
+
+  componentDidMount() {
+    this.props.fetchAdventureland()
+  }
+
+  render() {
+    return (
+      <div>
+        AdventurelandContainer
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({attractions: state.adventureland.list})
+
+export default connect(mapStateToProps, { fetchAdventureland })(AdventurelandContainer);
