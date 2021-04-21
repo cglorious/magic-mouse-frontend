3439da8136493c6d3a38451aa7f0fc11a2b3fdc2
Catherine Gloria <catherinegloria@gmail.com>
2021-04-20 21:06:53 -0700

    stub tomorrowland container

---
+++ src/containers/TomorrowlandContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchTomorrowland } from '../actions/loadAttractionsForLand'
+// import Fantasyland from '../components/Fantasyland';
+
+class TomorrowlandContainer extends Component {
+
+  componentDidMount() {
+    this.props.fetchTomorrowland()
+  }
+
+  render() {
+    return (
+      <div>
+        Container
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({attractions: state.tomorrowland.list})
+
+export default connect(mapStateToProps, { fetchTomorrowland })(TomorrowlandContainer);
