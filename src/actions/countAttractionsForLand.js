dd953c20a0ea11b31e44232a1a84becfb5645391
Catherine Gloria <catherinegloria@gmail.com>
2021-04-23 21:53:42 -0700

    create count actions

--- src/actions/countAttractionsForLand.js
+++ src/actions/countAttractionsForLand.js
@@ -25,3 +25,101 @@ export const decrementFantasyland = id => {
     payload: { id }
   }
 }
+
+export const incrementTomorrowland = id => {
+  return {
+    type: 'INCREMENT_TOMORROWLAND',
+    payload: { id }
+  }
+}
+
+export const decrementTomorrowland = id => {
+  return {
+    type: 'DECREMENT_TOMORROWLAND',
+    payload: { id }
+  }
+}
+
+export const incrementAdventureland = id => {
+  return {
+    type: 'INCREMENT_ADVENTURELAND',
+    payload: { id }
+  }
+}
+
+export const decrementAdventureland = id => {
+  return {
+    type: 'DECREMENT_ADVENTURELAND',
+    payload: { id }
+  }
+}
+
+export const incrementCritterCountry = id => {
+  return {
+    type: 'INCREMENT_CRITTERCOUNTRY',
+    payload: { id }
+  }
+}
+
+export const decrementCritterCountry = id => {
+  return {
+    type: 'DECREMENT_CRITTERCOUNTRY',
+    payload: { id }
+  }
+}
+
+export const incrementFrontierland = id => {
+  return {
+    type: 'INCREMENT_FRONTIERLAND',
+    payload: { id }
+  }
+}
+
+export const decrementFrontierland = id => {
+  return {
+    type: 'DECREMENT_FRONTIERLAND',
+    payload: { id }
+  }
+}
+
+export const incrementMainstreet= id => {
+  return {
+    type: 'INCREMENT_MAINSTREET',
+    payload: { id }
+  }
+}
+
+export const decrementMainstreet = id => {
+  return {
+    type: 'DECREMENT_MAINSTREET',
+    payload: { id }
+  }
+}
+
+export const incrementMickeysToontown = id => {
+  return {
+    type: 'INCREMENT_MICKEYSTOONTOWN',
+    payload: { id }
+  }
+}
+
+export const decrementMickeysToontown = id => {
+  return {
+    type: 'DECREMENT_MICKEYSTOONTOWN',
+    payload: { id }
+  }
+}
+
+export const incrementNola= id => {
+  return {
+    type: 'INCREMENT_NOLA',
+    payload: { id }
+  }
+}
+
+export const decrementNola = id => {
+  return {
+    type: 'DECREMENT_NOLA',
+    payload: { id }
+  }
+}
