bfb05f7c786799e96f16f0edad2a818c0589265e
Catherine Gloria <catherinegloria@gmail.com>
2021-04-26 20:17:49 -0700

    render star wars count

--- src/components/StarWarsAttraction.js
+++ src/components/StarWarsAttraction.js
@@ -5,16 +5,15 @@ import { connect } from 'react-redux';
 class StarWarsAttraction extends Component {
 
     render() {
-      const renderCount = id => {
-        return this.props.currentState.find(attraction => this.props.id === attraction.id).count
+
+      const { name, id, image, currentState } = this.props
+
+      const updateCount = id => {
+        return currentState.find(attraction => id === attraction.id).count
       }
 
-      const log = id => {
-        return this.props.currentState.find(attraction => id === attraction.id).count
-      }
-
-      const changeCount = () => {
-        document.getElementById(this.props.name).innerHTML = log(this.props.id)
+      const renderCount = () => {
+        document.getElementById(name).innerHTML = updateCount(id)
       }
 
     return(
@@ -24,21 +23,20 @@ class StarWarsAttraction extends Component {
           <img
             id="attraction-card-image"
             className="card-img-top"
-            src={this.props.image}
-            alt={this.props.name}
+            src={image}
+            alt={name}
             />
           <div className="card-body">
             <h5 className="card-title">{this.props.name}</h5>
               <div>
                 <h1 id={this.props.name}>0</h1>
                 <p>
-                  <button id={this.props.id} className="btn btn-primary" onClick={() => {
-                      this.props.incrementStarWars(this.props.id)
-                      changeCount()
-                      log(this.props.id)}
-                    }>+</button>    <button id={this.props.id} className="btn btn-primary" onClick={ () =>
-                    {this.props.decrementStarWars(this.props.id)
-                      changeCount()}
+                  <button id={id} className="btn btn-primary" onClick={() => {
+                      this.props.incrementStarWars(id)
+                      renderCount()}
+                    }>+</button>    <button id={id} className="btn btn-primary" onClick={ () =>
+                    {this.props.decrementStarWars(id)
+                      renderCount()}
                     }>-</button>
                 </p>
               </div>
