12725668f435bbea1814a2cd3716a18a139b3a64
Catherine Gloria <catherinegloria@gmail.com>
2021-05-09 17:16:42 -0700

    patch progress

--- src/actions/patchAttraction.js
+++ src/actions/patchAttraction.js
@@ -1,23 +1,59 @@
 export const patchStarWars = (attraction) => {
-  debugger
+  // console.log(attraction.count)
   return (dispatch) => {
-    fetch(`http://localhost:3000/api/v1/attractions/${attraction.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        'count': attraction.count
-      }),
+  fetch(`http://localhost:3000/api/v1/attractions/${attraction.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      "count": `${attraction.count}`
     })
-    // .then(resp => resp.json())
-    // .then(json => {
-    //   // dispatch({
-    //   //   type: 'UPDATE_COUNT',
-    //   //   payload: attraction.data
-    //   // })
-    //   console.log(`${json.data.attributes.name}: ${json.data.attributes.count}`)
-    // })
+  })
+  //   .then(resp => resp.json())
+  //   .then(json => {
+  //     dispatch({
+  //       type: 'UPDATE_COUNT',
+  //       payload: json.data
+  //     })
+  //   })
   }
 }
+
+// fetch(`http://localhost:3000/api/v1/attractions/${attraction.id}`, {
+//   method: 'PATCH',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     "count": `${attraction.count}`
+//   }),
+// })
+
+// fetch(`http://localhost:3000/api/v1/attractions/${attraction.id}`, {
+//   method: 'PATCH',
+//   headers: {
+//     'Content-Type': 'application/json-patch+json',
+//     'Accept': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     "op": "replace", "path": "/count", "value": `${attraction.count}`
+//   }),
+// })
+
+// fetch(`http://localhost:3000/api/v1/attractions`, {
+//   method: 'PUT',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Accept': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     "name": `${attraction.name}`,
+//     "image_url": `${attraction.image_url}`,
+//     "closure": false,
+//     "land_id": `${attraction.land_id}`,
+//     "count": `${attraction.count}`
+// })
+// })
