8008968d5abf895fdb928f032e29d2432a38f93c
Catherine Gloria <catherinegloria@gmail.com>
2021-05-16 17:35:31 -0700

    refactor reducers

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -2,8 +2,6 @@ const starWarsReducer = (state = {list: []}, action) => {
   switch(action.type) {
     case 'LOAD_ATTRACTIONS_FOR_STARWARS':
       return {...state, list: action.payload }
-    case 'ADD_ATTRACTION':
-      return {...state, list: [...state.list, action.payload]}
     default:
       return state;
   }
