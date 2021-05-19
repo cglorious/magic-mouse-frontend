50a48f0d077a2792dd621a4c0d8ebd5bb19e43b4
Catherine Gloria <catherinegloria@gmail.com>
2021-05-18 21:35:26 -0700

    notes

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import App from './App';
 //application communicates with Redux DevTools Chrome extension
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+//tell store to use the thunk middleware
 let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)) )
 
 ReactDOM.render(
