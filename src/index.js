03abdab99d1f8473f0346afd346908c8dfd6c863
Catherine Gloria <catherinegloria@gmail.com>
2021-05-19 06:58:55 -0700

    remove router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/rootReducer';
 
