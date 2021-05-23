4e2bac40a4140987de3d7bf907e55f93b274c94c
Catherine Gloria <catherinegloria@gmail.com>
2021-05-23 16:34:51 -0700

    search bar to lands container

--- src/containers/LandsContainer.js
+++ src/containers/LandsContainer.js
@@ -13,28 +13,24 @@ class LandsContainer extends Component {
     this.setState({
       [event.target.name]: event.target.value
     })
-    this.renderLands()
   }
 
-  //
   renderLands = () => {
-    const array = this.props.lands.filter( land => land.attributes.name.includes(this.state.search))
-    console.log(array)
+    if (this.state.search === '') {
+      return (
+        <Lands
+          lands={this.props.lands}
+          />
+      )
+    } else {
+      let query = this.props.lands.filter( land => land.attributes.name.includes(this.state.search) || land.attributes.name.toLowerCase().includes(this.state.search) )
+      return (
+        <Lands
+          lands={query}
+          />
+      )
+    }
   }
-
-  //lands.filter( land => land.attributes.name) - list of lands
-  //this.state.search - value of the input
-
-  //lands.filter( land => land.attributes.name.includes(this.state.search))
-
-  //compare the state to the letters
-  //strings
-
-  // input - search for lands, specific
-  // access name
-  // value - target?
-  // iterate through lands
-  // state
 
   componentDidMount() {
     this.props.fetchLands()
@@ -44,20 +40,21 @@ class LandsContainer extends Component {
 
     return (
       <div className="App">
-        <p>Enter land name below</p>
+        <div className='center'>
           <input
             name='search'
             value={this.state.search}
             onChange={this.handleChange}
+            type="search"
+            className="form-control ds-input"
+            placeholder="Search lands..."
             ></input>
+        </div>
+        <div>{this.renderLands()}</div>
       </div>
     );
   }
 }
-
-// <Lands
-//   lands={this.props.lands}
-//   />
 
 const mapStateToProps = state => ({lands: state.lands.list})
 
